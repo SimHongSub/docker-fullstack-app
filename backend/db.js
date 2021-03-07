dbe3423ce219406c1564962a1073239355eacf40
SimHongSub <ghdtjq2038@gmail.com>
2021-03-07 23:34:31 +0900

    travis ci deploy added

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql");
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: 'mysql',
-    user: 'root',
-    password: '1234',
-    database: 'myapp'
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_HOST
 });
 
 exports.pool = pool;
