c84a05e917181d3f49dfaa2ded78524a46139c36
SimHongSub <ghdtjq2038@gmail.com>
2021-03-07 23:59:33 +0900

    database port modified

--- backend/db.js
+++ backend/db.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_ROOT_PASSWORD,
     database: process.env.MYSQL_DATABASE,
-    port: process.env.MYSQL_HOST
+    port: process.env.MYSQL_PORT
 });
 
 exports.pool = pool;
