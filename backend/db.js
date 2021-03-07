a5e3c2d67feb9df250e09d5af0acdfdad22aa0b5
SimHongSub <ghdtjq2038@gmail.com>
2021-03-07 23:58:03 +0900

    database password modified

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const pool = mysql.createPool({
     connectionLimit: 10,
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
+    password: process.env.MYSQL_ROOT_PASSWORD,
     database: process.env.MYSQL_DATABASE,
     port: process.env.MYSQL_HOST
 });
